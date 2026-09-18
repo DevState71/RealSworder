9ee953add35d21c6fd079c14d6572379ee6ad2dc
Gabriel Fretter <fretterg@gmail.com>
2026-09-18 02:37:02 -0400

    Quick Merge

--- Source/Sworder/Private/Components/HealthComponent.cpp
+++ Source/Sworder/Private/Components/HealthComponent.cpp
@@ -16,20 +16,36 @@ UHealthComponent::UHealthComponent()
 	// ...
 }
 
+void UHealthComponent::TakeFlatDamage_Implementation(float DamageAmount, AActor* DamageCauser)
+{
+
+}
+
 float UHealthComponent::ProcessDamageType_Implementation(float DamageAmount, AActor* DamageCauser)
 {
 	UTagManager* SelfTagManager = GetOwner()->FindComponentByClass<UTagManager>();
 
 	UTagManager* AttackerTagManager = DamageCauser->FindComponentByClass<UTagManager>();
 
-	if(SelfTagManager && AttackerTagManager)
-	{
-		const FGameplayTagContainer& SelfTags = SelfTagManager->GetGameplayTagContainer();
-		const FGameplayTagContainer& AttackerTags = AttackerTagManager->GetGameplayTagContainer();
+	FGameplayTagContainer SelfTags;
+	FGameplayTagContainer AttackerTags;
 
+	if(SelfTagManager)
+	{
+		SelfTags = SelfTagManager->GetGameplayTagContainer();
+	}
+
+	if(AttackerTagManager)
+	{
+		AttackerTags = AttackerTagManager->GetGameplayTagContainer();
+	}
+
+	if (SelfTagManager && AttackerTagManager)
+	{
 		
 
-		if(SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element")) && AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element")))
+
+		if (SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element")) && AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element")))
 		{
 			UE_LOG(LogTemp, Warning, TEXT("Both the attacker and the target have Element tags!"));
 
@@ -44,7 +60,7 @@ float UHealthComponent::ProcessDamageType_Implementation(float DamageAmount, AAc
 				UE_LOG(LogTemp, Warning, TEXT("Fire is weak against Ice! Damage halved."));
 			}
 
-			if(AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Fire")) && SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Earth")))
+			if (AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Fire")) && SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Earth")))
 			{
 				DamageAmount *= 2.0f; // Fire is strong against Earth
 				UE_LOG(LogTemp, Warning, TEXT("Fire is strong against Earth! Damage doubled."));
@@ -55,7 +71,7 @@ float UHealthComponent::ProcessDamageType_Implementation(float DamageAmount, AAc
 				UE_LOG(LogTemp, Warning, TEXT("Earth is weak against Fire! Damage halved."));
 			}
 
-			if(AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Earth")) && SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Lightning")))
+			if (AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Earth")) && SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Lightning")))
 			{
 				DamageAmount *= 2.0f; // Earth is strong against Lightning
 				UE_LOG(LogTemp, Warning, TEXT("Earth is strong against Lightning! Damage doubled."));
@@ -66,7 +82,7 @@ float UHealthComponent::ProcessDamageType_Implementation(float DamageAmount, AAc
 				UE_LOG(LogTemp, Warning, TEXT("Lightning is weak against Earth! Damage halved."));
 			}
 
-			if(AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Lightning")) && SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Ice")))
+			if (AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Lightning")) && SelfTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Ice")))
 			{
 				DamageAmount *= 2.0f; // Lightning is strong against Ice
 				UE_LOG(LogTemp, Warning, TEXT("Lightning is strong against Ice! Damage doubled."));
@@ -78,7 +94,7 @@ float UHealthComponent::ProcessDamageType_Implementation(float DamageAmount, AAc
 			}
 
 		}
-	
+
 
 		if (AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Fire")))
 		{
@@ -92,15 +108,81 @@ float UHealthComponent::ProcessDamageType_Implementation(float DamageAmount, AAc
 		{
 			SelfTagManager->StatusRoll(FGameplayTag::RequestGameplayTag("Element.Lightning"));
 		}
-		else if (AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Earth")))
+
+	}
+	if (AttackerTagManager)
+	{
+		if (AttackerTags.HasTag(FGameplayTag::RequestGameplayTag("Element.Earth")))
 		{
-			AttackerTagManager->StatusRoll(FGameplayTag::RequestGameplayTag("Element.Earth"));
+			UHealthComponent* AttackerHealthComponent = DamageCauser->FindComponentByClass<UHealthComponent>();
+
+			if (AttackerHealthComponent)
+			{
+				AttackerTagManager->StatusRoll(FGameplayTag::RequestGameplayTag("Element.Earth"));
+			}
+			else
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Attacker does not have a HealthComponent, checking the owning Actor."));
+
+				AActor* AttackerActor = Cast<AActor>(DamageCauser->GetOwner());
+
+				if (AttackerActor)
+				{
+					AttackerHealthComponent = AttackerActor->FindComponentByClass<UHealthComponent>();
+
+					if (AttackerHealthComponent)
+					{
+						UTagManager* AttackerOwnerTagManager = AttackerActor->FindComponentByClass<UTagManager>();
+
+						if (AttackerOwnerTagManager)
+						{
+							AttackerOwnerTagManager->StatusRoll(FGameplayTag::RequestGameplayTag("Element.Earth"));
+						}
+						else
+						{
+							UE_LOG(LogTemp, Warning, TEXT("Attacker's owner does not have a TagManager."));
+						}
+					}
+					else
+					{
+						UE_LOG(LogTemp, Warning, TEXT("Attacker's owner does not have a HealthComponent."));
+					}
+				}
+				else
+				{
+					UE_LOG(LogTemp, Warning, TEXT("DamageCauser is not owned by an Actor."));
+				}
+
+			}
+
 		}
 	}
+
 	
 	return DamageAmount;
 }
 
+
+float UHealthComponent::DamageShield_Implementation(float DamageAmount, AActor* DamageCauser)
+{
+	if(ShieldHP > 0.0f)
+	{
+		if(DamageAmount <= ShieldHP)
+		{
+			
+			ShieldHP -= DamageAmount;
+			UE_LOG(LogTemp, Warning, TEXT("Shield absorbed all damage, remaining HP: %f"), ShieldHP);
+			DamageAmount = 0.0f;
+		}
+		else
+		{
+			DamageAmount -= ShieldHP;
+			UE_LOG(LogTemp, Warning, TEXT("Shield absorbed some damage, remaining damage: %f"), DamageAmount);
+			ShieldHP = 0.0f;
+		}
+	}
+	return DamageAmount;
+}
 
 // Called when the game starts
 void UHealthComponent::BeginPlay()
