1da122596754dc4133e9b053a899ef3470019e48
Gwilym <gwilymhernandez1@outlook.com>
2026-09-24 10:55:44 -0700

    Bugfix for the Can Attack boolean

--- Source/Sworder/Public/Characters/BasePlayer.h
+++ Source/Sworder/Public/Characters/BasePlayer.h
@@ -52,7 +52,7 @@ protected:
 
 	bool bIsAttacking : 1;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool bCanAttack : 1;
+	bool bCanAttack;
 
 	UFUNCTION()
 	void PlayerDamageCollision(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
